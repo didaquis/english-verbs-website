9f6ab00db54aa649ab2d21f5751bd4709d1267c3
Dídac García <didaquis@gmail.com>
2019-05-02 20:44:24 +0200

    Refactored code

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,17 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Layout from '../components/Layout'
 import Jumbotron from '../components/Jumbotron'
 import GroupOfCards from '../components/GroupOfCards'
 
 function Index() {
 	return (
-		<React.StrictMode>
-			<div className="bg-dark h-100 min-vh-100 d-flex flex-column">
-				<Header/>
-				<main className="container mb-3">
-					<div className="row">
-						<div className="col-lg-12 text-center">
-							<Jumbotron title={'📚'} subtitle={'A website with examples of use of tenses in English'} />
-						</div>
-					</div>
-					<GroupOfCards />
-				</main>
-				<Footer />
+		<Layout>
+			<div className="row">
+				<div className="col-lg-12 text-center">
+					<Jumbotron title={'📚'} subtitle={'A website with examples of use of tenses in English'} />
+				</div>
 			</div>
-		</React.StrictMode>
+			<GroupOfCards />
+		</Layout>
 	)
 }
 
