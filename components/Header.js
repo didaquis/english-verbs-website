b05dec6d1c09b5852ba191567b390a7590df97fc
Dídac García <didaquis@gmail.com>
2020-03-15 23:40:28 +0100

    Updated to Next.js 9.3

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
 			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 			<title>English Verbs</title>
 
-			<link href="/static/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-			<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/static/favicon.ico" />
-			<script src="/static/vendor/jquery/jquery.min.js"></script>
-			<script src="/static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+			<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/favicon.ico" />
+			<script src="/vendor/jquery/jquery.min.js"></script>
+			<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 		</Head>
 	)
 }
